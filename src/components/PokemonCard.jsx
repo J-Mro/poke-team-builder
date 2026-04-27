463514dc5178d60aa831719e9d13ef1961fd353a
J-Mro <j.k.mroueh@gmail.com>
2026-04-27 16:35:52 +0100

    add shiny pokemon toggle for sprites

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 export function PokemonCard(props) {
   const { pokemon } = props;
+  const [isShiny, setIsShiny] = useState(false);
   return (
     <div>
       <img
         className="pokemon-sprite"
-        src={pokemon.sprites.front_default}
+        src={
+          !isShiny ? pokemon.sprites.front_default : pokemon.sprites.front_shiny
+        }
         alt={`${pokemon.name} sprite`}
       />
       <p>{pokemon.name}</p>
       {pokemon.types.map((entry) => (
         <p>{entry.type.name}</p>
       ))}
+      <label>
+        shiny
+        <input
+          type="checkbox"
+          onChange={() => {
+            setIsShiny(!isShiny);
+          }}
+        />
+      </label>
     </div>
   );
 }
