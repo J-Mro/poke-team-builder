4bd3ea337dd8118aa89dc03ccf552e0d27006bb6
J-Mro <j.k.mroueh@gmail.com>
2026-04-28 14:22:04 +0100

    refactor Popup component to use index state (remove isOpen state entirely), abstract onClick and onSubmit callbacks, accomodate changes to getSinglePokemon util function

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,50 +3,46 @@ import { useState } from "react";
 import "./Popup.css";
 
 export function Popup(props) {
-  const { isOpen, setIsOpen, setPokemon } = props;
+  const { index, setIndex, pokemonTeam, setPokemonTeam } = props;
   const [search, setSearch] = useState("");
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
-  async function submit(e) {
-    const result = await getSinglePokemon(search);
-    setPokemon(result.data);
+  const handleClick = () => {
+    setIndex(null);
+  };
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const pokemon = await getSinglePokemon(search);
+      const updatedTeam = Array.from(pokemonTeam);
+      updatedTeam[index] = pokemon;
+      setPokemonTeam(updatedTeam);
+      setIndex(null);
+    } catch (err) {
+      console.log(err);
+    }
   }
-  if (isOpen) {
-    return (
-      <div className="popup">
-        <div className="popup-contents">
-          <button
-            className="close-btn"
-            onClick={() => {
-              setIsOpen(false);
-            }}
-          >
-            close
-          </button>
-          <h3>Choose your pokemon!</h3>
-          <form>
-            <label>
-              Use the search bar below to search for a pokemon by name:
-            </label>
-            <input
-              type="text"
-              placeholder="e.g. Bulbasaur"
-              onChange={handleChange}
-            />
-            <button
-              type="button"
-              onClick={() => {
-                submit();
-              }}
-            >
-              I choose you!
-            </button>
-          </form>
-        </div>
+
+  return (
+    <div className="popup">
+      <div className="popup-contents">
+        <button className="close-btn" onClick={handleClick}>
+          close
+        </button>
+        <h3>Choose your pokemon!</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Use the search bar below to search for a pokemon by name:
+          </label>
+          <input
+            type="text"
+            placeholder="e.g. Bulbasaur"
+            onChange={handleChange}
+          />
+          <button type="submit">I choose you!</button>
+        </form>
       </div>
-    );
-  } else {
-    return "";
-  }
+    </div>
+  );
 }
