576af8455dd668a285bf02c2aa4f4dab9d86a369
J-Mro <j.k.mroueh@gmail.com>
2026-04-28 10:54:40 +0100

    refactor getSinglePokemon to return data from axios get

--- src/utils/getSinglePokemon.js
+++ src/utils/getSinglePokemon.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export async function getSinglePokemon(name) {
-  const pkmn = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  return pkmn;
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemon = response.data;
+  return pokemon;
 }
